507d571cb9f3005010bb6bbbc41811c7dfe0db19
Mugdha Hema Sri <mugdhahemasri08@gmail.com>
2026-01-07 11:52:45 +0530

    Refactor ask function to use user_prompt

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 from flask import Flask, request, jsonify
 from google import genai
-from dotenv import load_dotenv
+from google.genai import types
 from flask_cors import CORS
 import os
-
+from dotenv import load_dotenv
 load_dotenv()
-
 app = Flask(__name__)
-CORS(app)   # <-- Allow requests from frontend
+CORS(app)
 
+# Create client (API key must be in environment)
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+# System prompt
+SYSTEM_PROMPT = "You'r name is Hema Sri, you are working as Genai engineer in infosys, your expertize is python genai, aws, azure ai foundry"
 
 @app.route("/api/ask", methods=["POST"])
 def ask():
     data = request.json
-    prompt = data.get("prompt", "")
+    user_prompt = data.get("prompt", "")
 
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=prompt
+        config=types.GenerateContentConfig(
+            system_instruction=SYSTEM_PROMPT
+        ),
+        contents=user_prompt
     )
 
-    return jsonify({"response": response.text})
+    return jsonify({
+        "response": response.text
+    })
 
 @app.route("/", methods=["GET"])
 def home():
